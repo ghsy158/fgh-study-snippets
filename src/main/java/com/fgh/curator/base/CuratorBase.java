2cba6a2de4c6214a46539d343ca0529cdee7fc41
ghsy158 <fgh326@126.com>
2016-07-13 11:42:35 +0800

    update

--- src/main/java/com/fgh/curator/base/CuratorBase.java
+++ src/main/java/com/fgh/curator/base/CuratorBase.java
@@ -1,16 +1,12 @@
 package com.fgh.curator.base;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.List;
 
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.framework.api.BackgroundCallback;
-import org.apache.curator.framework.api.CuratorEvent;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
 
 public class CuratorBase {
 
@@ -20,6 +16,7 @@ public class CuratorBase {
 
 	public static void main(String[] args) throws Exception {
 
+		//重试策略 初始时间1s 重试10次
 		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 10);
 
 		CuratorFramework cf = CuratorFrameworkFactory.builder().connectString(CONNECT_ADDR).sessionTimeoutMs(TIME_OUT)
@@ -35,23 +32,36 @@ public class CuratorBase {
 //		cf.setData().forPath("/curator/c1","3423".getBytes());
 		
 		
-		ExecutorService pool = Executors.newCachedThreadPool();
+		//线程池的目的 批量操作节点的时候
+//		ExecutorService pool = Executors.newCachedThreadPool();
+//		
+//		cf.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)
+//		.inBackground(new BackgroundCallback() {
+//			
+//			@Override
+//			public void processResult(CuratorFramework client, CuratorEvent event) throws Exception {
+//				System.out.println("code："+event.getResultCode());
+//				System.out.println("type："+event.getType());
+//				System.out.println("线程为："+Thread.currentThread().getName());
+//			}
+//		},pool)
+//		.forPath("/curator/c2","c2内容".getBytes());
+//		
+//		System.out.println("主线程:"+Thread.currentThread().getName());
+//		
+//		Thread.sleep(Integer.MAX_VALUE);
 		
-		cf.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)
-		.inBackground(new BackgroundCallback() {
-			
-			@Override
-			public void processResult(CuratorFramework client, CuratorEvent event) throws Exception {
-				System.out.println("code："+event.getResultCode());
-				System.out.println("type："+event.getType());
-				System.out.println("线程为："+Thread.currentThread().getName());
-			}
-		},pool)
-		.forPath("/curator/c2","c2内容".getBytes());
 		
-		System.out.println("主线程:"+Thread.currentThread().getName());
+		List<String> list  = cf.getChildren().forPath("/curator");
+		for(String path:list){
+			System.out.println(path);
+		}
 		
-		Thread.sleep(Integer.MAX_VALUE);
+		
+		Stat stat = cf.checkExists().forPath("/curator/c1");
+		System.out.println(stat);
+		
+		cf.delete().deletingChildrenIfNeeded().forPath("/curator");
 		
 		cf.close();
 	
